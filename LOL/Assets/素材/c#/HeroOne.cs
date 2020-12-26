2bc9f4ef917ab9f956dc41ccd7f3087cbffd7f65
cges1134 <73698355+cges1134@users.noreply.github.com>
2020-12-26 20:20:17 +0800

    角色動畫放到按鍵

---
+++ LOL/Assets/素材/c#/HeroOne.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class HeroOne : MonoBehaviour
+{
+    [Header("角色資料")]
+    public Herocontrol Date;
+    protected Animator ani;
+    private float [] skillTimer = new float[4];
+    private bool[] skillStart = new bool[4];
+
+    protected virtual void Awake()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    public void Move()
+    {
+
+    }
+
+    public void skill1()
+    {
+        ani.SetTrigger("第一招");
+    }
+
+    public void skill2()
+    {
+        ani.SetTrigger("第二招");
+    }
+
+    public void skill3()
+    {
+        ani.SetTrigger("第三招");
+    }
+
+    public void skill4()
+    {
+        ani.SetTrigger("大招");
+    }
+}
---
+++ LOL/Assets/素材/c#/HeroPlayer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroPlayer : HeroOne
+{
+    private Button btnskill1;
+    private Button btnskill2;
+    private Button btnskill3;
+    private Button btnskill4;
+
+    private Image[] imgSkills = new Image[4];
+    private Text[] textSkills = new Text[4];
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        SetButtonUI();
+    }
+
+
+    private void SetButtonUI()
+    {
+
+        btnskill1 = GameObject.Find("技能1").GetComponent<Button>();
+        btnskill2 = GameObject.Find("技能2").GetComponent<Button>();
+        btnskill3 = GameObject.Find("技能3").GetComponent<Button>();
+        btnskill4 = GameObject.Find("技能4").GetComponent<Button>();
+
+        btnskill1.onClick.AddListener(skill1);
+        btnskill2.onClick.AddListener(skill2);
+        btnskill3.onClick.AddListener(skill3);
+        btnskill4.onClick.AddListener(skill4);
+        for (int i = 0; i < 4; i++)
+        {
+            imgSkills[i] = GameObject.Find("技能圖片" + (i + 1)).GetComponent<Image>();
+            textSkills[i] = GameObject.Find("技能數字" + (i + 1)).GetComponent<Text>();
+            imgSkills[i].sprite = Date.skills[i].image;
+            textSkills[i].text = "";
+        }
+    }
+}
+
+
